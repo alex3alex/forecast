2e5375cf9370ac244c10d63a5db444ab0388c3a3
Ashish Dixit <tundal45@gmail.com>
2015-07-31 19:31:35 -0500

    Button fetching new forecast & updating!

--- app/adapters/forecast.js
+++ app/adapters/forecast.js
@@ -3,6 +3,20 @@ import ajax from 'ic-ajax';
 
 export default Ember.Object.extend({
   find: function(name, coords) {
-    return ajax("/" + coords)
+    return ajax("/" + coords).then(function(result) {
+      var forecast = {
+        latitude: result.latitude,
+        longitude: result.longitude,
+        timezone: result.timezone,
+        temperature: result.currently.temperature,
+        apparentTemperature: result.currently.apparentTemperature,
+        humidity: result.currently.humidity,
+        summary: result.currently.summary,
+        precipIntensity: result.currently.precipIntensity,
+        precipProbability: result.currently.precipProbability
+      };
+
+      return forecast;
+    });
   }
 });
--- app/routes/forecast.js
+++ app/routes/forecast.js
@@ -3,11 +3,21 @@ import ForecastAdapter from 'forecast/adapters/forecast';
 
 export default Ember.Route.extend({
   model: function() {
+    return this.adapter.find('forecast', this._getRandomCoords());
+  },
+  adapter: ForecastAdapter.create(),
+  actions: {
+    fetchForecast: function() {
+      var self = this;
+      this.adapter.find('forecast', this._getRandomCoords()).then(function(newForecast) {
+        console.log(newForecast);
+        self.controller.set('model', newForecast);
+      });
+    }
+  },
+  _getRandomCoords: function() {
     var lat = (Math.random()*180 - 90).toFixed(3);
     var lng = (Math.random()*360 - 180).toFixed(3);
-    var coords = lat + "," + lng;
-    var adapter = ForecastAdapter.create();
-
-    return adapter.find('forecast', coords);
+    return lat + "," + lng;
   }
 });
